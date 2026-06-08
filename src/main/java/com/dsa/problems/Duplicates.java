190b8ff7eef4870a88c7c284d98a39e7e2ab4f7a
manu-005 <manojbetadur@gmail.com>
2026-06-09 02:09:30 +0530

    Duplicates in an array

--- src/main/java/com/dsa/problems/Duplicates.java
+++ src/main/java/com/dsa/problems/Duplicates.java
@@ -1,30 +1,37 @@
 package com.dsa.problems;
 
-import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Scanner;
 
 public class Duplicates {
 
     public static void main(String[] args) {
 
         // return true if an array having duplicates, if not return false
+        Scanner sc = new Scanner(System.in);
 
-        int[] arr = {1, 4, 2, 5};
+        System.out.println("enter size:");
+        int num = sc.nextInt();
 
-        Solution solution = new Solution();
+        System.out.println(num);
+        int[] arr = new int[num];
 
-        System.out.println(solution.checkDuplicates(arr));
+        System.out.println("enter elements");
+        for (int i = 0; i < num; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println();
+        int[] arr2 = {1, 4, 4, 2, 5};
+
+        System.out.println(checkDuplicates(arr2));
     }
-}
 
-class Solution {
-
-    public boolean checkDuplicates(int[] arr) {
+    public static boolean checkDuplicates(int[] arr) {
 
         HashSet<Integer> set = new HashSet<>();
 
         for (int n : arr) {
-
             if (set.contains(n)) {
                 return true;
             }
