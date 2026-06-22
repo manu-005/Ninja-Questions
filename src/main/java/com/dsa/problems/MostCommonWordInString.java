78f0317db4d96264019579e442a79a9aae946567
manu-005 <manojbetadur@gmail.com>
2026-06-22 23:57:17 +0530

    print duplicates in list

---
+++ src/main/java/com/dsa/problems/MostCommonWordInString.java
@@ -0,0 +1,21 @@
+package com.dsa.problems;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class MostCommonWordInString {
+    public static void main(String[] args) {
+
+        String str = "mmmmmmmmmmmmmmmmmmmmmmndhkdndbbbbbb";
+
+      Map.Entry<Character,Long> v =  str.chars().mapToObj(c-> (char) c)
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+                .entrySet().stream().max(Map.Entry.comparingByValue()).get();
+        System.out.println(v);
+
+    }
+
+}
