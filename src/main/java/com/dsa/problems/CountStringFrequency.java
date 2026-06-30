90c7ceb65426f4a8d70c3c4918b0d9f1201067e3
manu-005 <manojbetadur@gmail.com>
2026-06-30 23:11:27 +0530

    print duplicates in list

--- src/main/java/com/dsa/problems/CountStringFrequency.java
+++ src/main/java/com/dsa/problems/CountStringFrequency.java
@@ -10,11 +10,12 @@ public class CountStringFrequency {
         // Convert a List of Strings into a Map with word
         // count using Java Streams (key should word, value should be it's count)
 
-        List<String> strs = Arrays.asList("manoj", "bharat","manoj", "manoj","manu","bharat");
+        List<String> strs = Arrays.asList("manoj", "bharat", "manoj", "manoj", "manu", "bharat");
 
-       Map<String, Long> wordCount = strs.stream().collect(Collectors.groupingBy(word -> word,Collectors.counting()));
+        Map<String, Long> wordCount = strs.stream().collect(Collectors.groupingBy(word -> word, Collectors.counting()));
 
-       Map<String,Long> re =strs.stream().collect(Collectors.groupingBy(e->e,Collectors.counting()));
+        Map<String, Long> re = strs.stream().collect(Collectors.groupingBy(e -> e, Collectors.counting()));
         System.out.println(wordCount);
         System.out.println(re);
-    }}
+    }
+}
